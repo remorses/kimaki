02bf717d6b498f4c9a3edaf8c34c0e7cfa13cc32
remorses <beats.by.morse@gmail.com>
2025-09-02 19:39:49 +0200

    nn

--- cli/src/file-logger.tsx
+++ cli/src/file-logger.tsx
@@ -1,21 +1,18 @@
 import * as fs from 'fs'
+import { dump } from 'js-yaml'
 
 const logFilePath = '/Users/morse/Documents/GitHub/kimakivoice/app.log'
 
 // Ensure the log file exists
 if (!fs.existsSync(logFilePath)) {
-    fs.writeFileSync(logFilePath, '')
+  fs.writeFileSync(logFilePath, '')
 }
 
 export const logger = {
-    log(...args: any[]) {
-        const timestamp = new Date().toISOString()
-        const message = `[${timestamp}] ${args
-            .map((arg) =>
-                typeof arg === 'object' ? JSON.stringify(arg) : String(arg),
-            )
-            .join(' ')}\n`
-
-        fs.appendFileSync(logFilePath, message)
-    },
+  log(...args: any[]) {
+    const timestamp = new Date().toISOString()
+    const payload = { timestamp, messages: args.map((arg) => (typeof arg === 'object' ? arg : String(arg))) }
+    const yaml = dump(payload)
+    fs.appendFileSync(logFilePath, `---\n${yaml}\n`)
+  },
 }
