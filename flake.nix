aa059c8b33cbe94492c84e079afd9e44c1a274f5
Brian Li <brnli7@gmail.com>
2026-02-28 18:15:39 +0000

    add nix

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "kimaki dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs_24
+              pkgs.pnpm
+              pkgs.python3
+              pkgs.pkg-config
+              pkgs.gnumake
+              pkgs.gcc
+              pkgs.sqlite
+            ];
+          };
+        });
+    };
+}
