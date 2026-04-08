620cb703c3da3d3a7b8b7e58ab9a3c2b94f4b631
Tommy D. Rossi <beats.by.morse@gmail.com>
2026-04-08 09:39:25 +0200

    Fix Anthropic Max Subscription third party detection. Max subscription works again

--- cli/src/anthropic-auth-plugin.ts
+++ cli/src/anthropic-auth-plugin.ts
@@ -743,6 +743,14 @@ async function getFreshOAuth(
 
 const AnthropicAuthPlugin: Plugin = async ({ client }) => {
   return {
+    "experimental.chat.system.transform": async (input, output) => {
+      if (input.model.providerID !== ('anthropic')) return
+      const opencodePromptPart = output.system.findIndex(x => x?.includes('https://github.com/anomalyco/opencode'))
+      // Remove the OpenCode system prompt part if present
+      if (opencodePromptPart !== -1) {
+        output.system.splice(opencodePromptPart, 1)
+      }
+    },
     auth: {
       provider: 'anthropic',
       async loader(
